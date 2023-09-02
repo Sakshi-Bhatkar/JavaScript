ad65e1b7efb99a7a7519d79cba33ce58d2cd9f85
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-09-02 23:50:36 +0530

    data types
    //----------primitive data types-------------------------
      // String = ""
      //boolean = true/false
      //null = stand alone value
      //undefined= when you declare a variable but doesn't specify value
      //symbol =  unique
    
    
    
    //-------------------object-----------------------
    console.log(typeof undefined); // this is a type
    console.log(typeof null); // this is a object

---
+++ 2_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as a new version
+let name = "Sakshi - String Data Type"
+let age = 25 //Number
+let isLoggedIn = false //boolean
+  console.log(age);
+
+//----------primitive data types-------------------------
+  // String = ""
+  //boolean = true/false
+  //null = stand alone value
+  //undefined= when you declare a variable but doesnt sepcify value
+  //symbol =  unique
+
+//-------------------object-----------------------
+console.log(typeof undefined); // this is a type
+console.log(typeof null); // this is a object

633a556538a6d02eceff44ba3b1e46ca5ed2cd0c
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-09-03 00:09:36 +0530

    conversion and operations
    convert string to number and number to Boolean and vise versa

---
+++ 3_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = 33;
+console.log(score);
+console.log(typeof(score)); // as a method
+console.log(typeof score);
+
+let valueInNumber = String(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" --- 33
+//"33svdf" ---NaN not a number
+//true---1 false----0
+/*let isLoggedIn = 1
+  let booleanIsLoggedIn = Boolean (isLoggedIn)
+  console.log(booleanIsLoggedIn)
+*/ 
+/* let score = 33
+let someString = String(score)
+console.log(typeof someString);
+console.log(someString);
+*/

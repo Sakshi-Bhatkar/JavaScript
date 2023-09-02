5c638c2e3f2c70167b1636648bc2a98206ad60e3
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-09-02 23:23:22 +0530

    Variables in JS
    let , const , var variables also console.table([ -------]);

---
+++ 1_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123
+let accountEmail = "sakshi@gmail.com"
+var accountPass = "S@kshi"
+accountCity = "Pune"
+
+
+ // const accountId = 123;  --------------->   const cannot be redeclared 
+accountEmail = "sakshibhatkar@gmail.com"
+accountPass = "S@"
+accountCity = "Amr"
+console.table([accountId,accountEmail,accountPass,accountCity]) // console.table prints in table format

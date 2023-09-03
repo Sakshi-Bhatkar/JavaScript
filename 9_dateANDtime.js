e209cf45c475a41abde64883eba4493576a9f1d3
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-09-03 23:45:23 +0530

    date and time
    date and time

---
+++ 9_dateANDtime.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+// let myCreateDate = new Date (2023,0,13)
+// let myCreateDate = new Date (2023,0,13 ,5,3)
+// let myCreateDate = new Date ("2023-01-13")
+let myCreateDate = new Date ("01-13-2023")
+let myTimeStamp =Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreateDate.getTime());
+  let newDate =new Date
+//  console.log(newDate);
+//  console.log(newDate.getMonth());
+
+
+newDate.toLocaleString('default', {
+    weekday:"long"
+
+})

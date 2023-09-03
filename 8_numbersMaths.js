3965d3a1affdd07824317f273b82d728092630ca
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-09-03 23:11:34 +0530

    Math functions
    Math functions

---
+++ 8_numbersMaths.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score);
+ const balance = new Number (100.34)
+ console.log(balance);
+
+ console.log(balance.toString().length);
+
+ const otherNo = 2.2344
+ console.log(otherNo.toPrecision(3));
+
+ const three = 1000000000
+ console.log(three.toLocaleString('en-IN'));
+
+//  Maths
+
+console.log(Math.abs(-4)); // makes negative no as positive but positive will always remain positive
+console.log(Math.round(4.6)); 
